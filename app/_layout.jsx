85627b4811fe31c81f152767c3fdc339e6d68a95
Kushan2k <kushangayantha001@gmail.com>
2023-09-28 11:39:49 +0530

    app naviagtion added

---
+++ app/_layout.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+import { Stack } from 'expo-router'
+
+export default function layout() {
+  return (
+    <Stack/>
+      
+  )
+}
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,11 @@
+import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Stack.Screen  options={{title:"Welcome"}}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <Text>Hello world</Text>
